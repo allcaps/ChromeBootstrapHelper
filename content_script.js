e2a43ad0f269b984d7723b2d9e3153d7a194aee6
Coen van der Kamp <c.van.der.kamp@zestsoftware.nl>
2015-11-17 19:59:40 +0100

    Fix 1: Toggle BS helper on/off.

--- content_script.js
+++ content_script.js
@@ -1,18 +1,27 @@
-var elm = document.createElement('div');
-elm.style.cssText = 'position:fixed;'
-				  + 'top:3px;'
-				  + 'left:3px;'
-				  + 'z-index:9999;'
-				  + 'padding:.25em;'
-				  + 'border:1px solid black;'
-				  + 'background:white;color:black;';
+var elm = document.createElement('div'),
+sizes = ['xs', 'sm', 'md', 'lg'],
+uid = 'chromebootstraphelper',
+exist = document.getElementById(uid),
+i;
 
-var sizes = ['xs', 'sm', 'md', 'lg']
+elm.id = uid;
+elm.style.cssText = 'position:fixed;'
+                  + 'top:3px;'
+                  + 'left:3px;'
+                  + 'z-index:9999;'
+                  + 'padding:.25em;'
+                  + 'border:1px solid black;'
+                  + 'background:white;color:black;';
+
 for (i = 0; i < sizes.length; i++) { 
-	var item = document.createElement('div');
-	item.className = 'visible-' + sizes[i];
-	item.textContent = sizes[i]; 
+  var item = document.createElement('div');
+  item.className = 'visible-' + sizes[i];
+  item.textContent = sizes[i];
     elm.appendChild(item);
 }
 
-document.body.appendChild(elm);
+if (exist === null) {
+  document.body.appendChild(elm);
+} else {
+  exist.parentNode.removeChild(exist);
+}
